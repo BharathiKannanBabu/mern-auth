1bfa72a2f80cc96fa39b5e9a9b656ed744476a55
BharathiKannanBabu <bharathikannan130552055@gmail.com>
2024-08-09 10:31:00 +0530

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,7 +1,11 @@
 import express from "express";
+import connectMongoDB from "./db/connectMongoDB.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 
 app.listen(3000, () => {
     console.log('Server is listening to the post is 3000');
+    connectMongoDB();
 })
