0e140998633d7c7097df7920f7f93b78cbd3aec8
BharathiKannanBabu <bharathikannan130552055@gmail.com>
2024-08-11 20:28:10 +0530

    create sign up api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import connectMongoDB from "./db/connectMongoDB.js";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.routes.js"
+import authRoutes from "./routes/auth.routes.js"
 dotenv.config();
 
 const app = express();
@@ -10,5 +11,7 @@ app.listen(3000, () => {
     console.log('Server is listening to the post is 3000');
     connectMongoDB();
 })
+app.use(express.json())
 
 app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
